f9d05367d0a8376759ff253e05bc427fffdc0784
usmanali001 <usman.ali@oip.com.pk>
2023-03-15 18:31:26 +0500

    changess of 10 node

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         hello worlsd
+         hello usman
         </p>
         <a
           className="App-link"
